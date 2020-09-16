961dee324903d5422451e0034d1a40727dfa185a
jceastling <66752350+jceastling@users.noreply.github.com>
2020-09-16 10:16:16 -0700

    Update freddiebot.js

--- freddiebot.js
+++ freddiebot.js
@@ -213,18 +213,6 @@ function helpCommand(arguments, receivedMessage) {
     }
 }
 
-function multiplyCommand(arguments, receivedMessage) {
-    if (arguments.length < 2) {
-        receivedMessage.channel.send("Not enough values to multiply. Try `!multiply 2 4 10` or `!multiply 5.2 7`")
-        return
-    }
-    let product = 1
-    arguments.forEach((value) => {
-        product = product * parseFloat(value)
-    })
-    receivedMessage.channel.send("The product of " + arguments + " multiplied together is: " + product.toString())
-}
-
 // Get your bot's secret token from:
 // https://discordapp.com/developers/applications/
 // Click on your application -> Bot -> Token -> "Click to Reveal Token"
