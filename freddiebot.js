688f245d642f12f4971680fc97d72551a78219f2
jceastling <66752350+jceastling@users.noreply.github.com>
2021-02-25 11:58:19 -0800

    Update freddiebot.js

--- freddiebot.js
+++ freddiebot.js
@@ -254,11 +254,6 @@ function processCommand(receivedMessage) {
 	    receivedMessage.channel.send(final[Math.floor(Math.random() * final.length)])
     } else if (primaryCommand == "help") {
         receivedMessage.channel.send("I'll `sing`, `talk`, or give you a `kiss`, dear; I'll `greet` or `compliment` you too! But don't forget to start with &&.")
-	} else if (primaryCommand == ) {
-	message.react('??');
-	message.react('??');
-	message.react('??');
-}
     } else {
         receivedMessage.channel.send("I haven't a clue what you want, darling. Try `&&help` instead.")
     }
